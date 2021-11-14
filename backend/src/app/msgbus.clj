12d011d58d3ea080ea2300f10e88ca06a0767c92
Josh Soref <jsoref@users.noreply.github.com>
2021-11-14 01:13:49 -0500

    spelling: sliding

--- backend/src/app/msgbus.clj
+++ backend/src/app/msgbus.clj
@@ -181,7 +181,7 @@
                   (reify RedisPubSubListener
                     (message [it pattern topic message])
                     (message [it topic message]
-                      ;; There are no back pressure, so we use a slidding
+                      ;; There are no back pressure, so we use a sliding
                       ;; buffer for cases when the pubsub broker sends
                       ;; more messages that we can process.
                       (let [val {:topic topic :message (blob/decode message)}]
