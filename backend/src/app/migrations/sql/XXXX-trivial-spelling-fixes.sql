0295fcf3407f87fc39467b18bb9b5ee5ddb71c94
Josh Soref <jsoref@users.noreply.github.com>
2021-11-14 02:56:45 -0500

    spelling: object

--- backend/src/app/migrations/sql/XXXX-trivial-spelling-fixes.sql
+++ backend/src/app/migrations/sql/XXXX-trivial-spelling-fixes.sql
@@ -1,2 +1,3 @@
 alter index color_colection__team_id__idx to color_collection__team_id__idx;
 alter index icon_colection__team_id__idx to icon_collection__team_id__idx;
+alter index media_bject__file_id__idx rename to media_object__file_id__idx;
